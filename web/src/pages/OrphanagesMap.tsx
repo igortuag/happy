91c9a4cf9ded6c647656fd47dca567bf64e78cea
Igor Tuag <igortuag@gmail.com>
2020-10-12 11:20:26 -0300

    Layout and struct OrphanagesMap

--- web/src/pages/OrphanagesMap.tsx
+++ web/src/pages/OrphanagesMap.tsx
@@ -1,7 +1,33 @@
 import React from 'react';
+import { FiPlus } from 'react-icons/fi';
+import { Link } from 'react-router-dom';
+
+import '../styles/pages/orphanages-map.css';
+
+import mapMarkerImg from '../images/map-marker.svg'
 
 function OrphanagesMap() {
-  return <h1>Mapa de Orfanatos</h1>;
+  return (
+    <div id="page-map">
+      <aside>
+        <header>
+          <img src={mapMarkerImg} alt=""/>
+
+          <h2>Escolha um orfanato no mapa</h2>
+          <p>Muitas crianças estão esperando a sua visita :)</p>
+        </header>
+
+        <footer>
+          <strong>Vila Velha</strong>
+          <span>Espírito Santo</span>
+        </footer>
+      </aside>
+
+      <Link to="" className="create-orphanage">
+        <FiPlus size={32} color="#FFF"/>
+      </Link>
+    </div>
+  );
 }
 
 export default OrphanagesMap;
