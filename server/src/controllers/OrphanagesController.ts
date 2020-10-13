84ac8cffe9d6d4c1c7c4f2d1b21eb2512819346a
Igor Tuag <igortuag@gmail.com>
2020-10-13 19:31:07 -0300

    Modularization, abstracting the code in controllers

---
+++ server/src/controllers/OrphanagesController.ts
@@ -0,0 +1,33 @@
+import { Request, Response } from 'express';
+import { getRepository } from 'typeorm';
+import Orphanages from '../models/Orphanages';
+
+export default {
+  async create(req: Request, res: Response) {
+    const {
+      name,
+      latitude,
+      longitude,
+      about,
+      instructions,
+      opening_hours,
+      open_on_weekends,
+    } = req.body;
+
+    const orphanagesRepository = getRepository(Orphanages);
+
+    const orphanage = orphanagesRepository.create({
+      name,
+      latitude,
+      longitude,
+      about,
+      instructions,
+      opening_hours,
+      open_on_weekends,
+    });
+
+    await orphanagesRepository.save(orphanage);
+
+    return res.status(201).json(orphanage);
+  },
+};
--- server/src/server.ts
+++ server/src/server.ts
@@ -1,39 +1,11 @@
 import express from 'express';
-import { getRepository } from 'typeorm';
-import Orphanages from './models/Orphanages';
-
 import './database/conection';
+
+import rotues from './routes';
 
 const app = express();
 
 app.use(express.json());
-
-app.post('/orphanages', async (req, res) => {
-  const {
-    name,
-    latitude,
-    longitude,
-    about,
-    instructions,
-    opening_hours,
-    open_on_weekends,
-  } = req.body;
-
-  const orphanagesRepository = getRepository(Orphanages);
-
-  const orphanage = orphanagesRepository.create({
-    name,
-    latitude,
-    longitude,
-    about,
-    instructions,
-    opening_hours,
-    open_on_weekends,
-  });
-
-  await orphanagesRepository.save(orphanage);
-
-  return res.json({ message: 'Hello World' });
-});
+app.use(rotues);
 
 app.listen(3333);
