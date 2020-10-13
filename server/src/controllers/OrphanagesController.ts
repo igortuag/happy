679faecad9821e3361eebf1cd6cef67b71401940
Igor Tuag <igortuag@gmail.com>
2020-10-13 20:40:07 -0300

    Integration views and url to server images

--- server/src/controllers/OrphanagesController.ts
+++ server/src/controllers/OrphanagesController.ts
@@ -1,14 +1,17 @@
 import { Request, response, Response } from 'express';
 import { getRepository } from 'typeorm';
 import Orphanages from '../models/Orphanage';
+import orphanagesView from '../views/orphanages_view';
 
 export default {
   async index(req: Request, res: Response) {
     const orphanagesRepository = getRepository(Orphanages);
 
-    const orphanages = await orphanagesRepository.find();
+    const orphanages = await orphanagesRepository.find({
+      relations: ['images'],
+    });
 
-    return res.json(orphanages);
+    return res.json(orphanagesView.renderMany(orphanages));
   },
 
   async show(req: Request, res: Response) {
@@ -16,9 +19,11 @@ export default {
 
     const orphanagesRepository = getRepository(Orphanages);
 
-    const orphanages = await orphanagesRepository.findOneOrFail(id);
+    const orphanages = await orphanagesRepository.findOneOrFail(id, {
+      relations: ['images'],
+    });
 
-    return res.json(orphanages);
+    return res.json(orphanagesView.render(orphanages));
   },
 
   async create(req: Request, res: Response) {
